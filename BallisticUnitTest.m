fe9e9e5005d70d1f6642d15ee49e0af280cc868d
Jeffrey J. Early <jeffrey@jeffreyearly.com>
2015-09-25 16:40:18 -0700

    Adding a test for a ballistic (constant forcing) trajectory

---
+++ BallisticUnitTest.m
@@ -0,0 +1,88 @@
+g = -9.8;
+v0 = 500;
+u0 = 500;
+t_max = -2*v0/g;
+
+N = 7; % Number of observations
+t=linspace(0,t_max,N)';
+x=u0*t;
+y=g*t.*t/2 + v0*t;
+
+
+dx=10*ones(size(x));
+dy=10*ones(size(y));
+a0=2*abs(g);
+M=30; % Number of interior knot points (need two extras for end points)
+W=eye(N);
+S = 5;
+[m_x,m_y,Cm_x,Cm_y,X,V,A,J] = drifter_fit_lagrangian(t,x,y,dx,dy,W,M,S,a0,0,@(z)(z));
+
+Nt=51;
+M = M+2*floor(S/2);
+tt = linspace(0,t_max,Nt)';
+t_knot = (t(end)-t(1))/(M-S);
+
+if S == 3
+    addpath('./cubic_splines');
+    spline = @(t) cspline(t);
+    spline_t = @(t) cspline_t(t);
+    spline_tt = @(t) cspline_tt(t);
+    spline_ttt = @(t) cspline_ttt(t);
+elseif S == 5
+    addpath('./quintic_splines');
+    spline = @(t) qspline(t);
+    spline_t = @(t) qspline_t(t);
+    spline_tt = @(t) qspline_tt(t);
+    spline_ttt = @(t) qspline_ttt(t);
+else
+    disp('Whoops! I only know how to deal with splines of order 3 and 5.')
+    return;
+end
+
+X = zeros(Nt,M);
+V = zeros(Nt,M);
+A = zeros(Nt,M);
+J = zeros(Nt,M);
+for i=1:Nt
+    for j=1:M
+        t_norm=(tt(i)-t(1))/t_knot - (j - 1 - floor(S/2));
+        X(i,j)=spline(t_norm);
+        V(i,j)=spline_t(t_norm);
+        A(i,j)=spline_tt(t_norm);
+        J(i,j)=spline_ttt(t_norm);
+    end
+end
+V = V/t_knot;
+A = A/(t_knot^2);
+J = J/(t_knot^3);
+
+xi = X*m_x;
+eta = X*m_y;
+
+% xi = xi(1:M/2);
+% eta = eta(1:M/2);
+
+figure
+plot(xi,eta)
+hold on
+scatter(xi,eta)
+scatter(x,y,5^2,'k')
+
+figure
+subplot(4,1,1)
+plot(tt,[X*m_x, X*m_y])
+xlim([min(t) max(t)])
+ylabel('distance (m)')
+subplot(4,1,2)
+plot(tt,[V*m_x, V*m_y])
+xlim([min(t) max(t)])
+ylabel('speed (m/s)')
+subplot(4,1,3)
+plot(tt,[A*m_x, A*m_y])
+xlim([min(t) max(t)])
+ylabel('acceleration (m/s^2)')
+subplot(4,1,4)
+plot(tt,[J*m_x, J*m_y])
+xlim([min(t) max(t)])
+ylabel('jerk (m/s^3)')
+xlabel('time (hours)')
