1b81d43961d26615ed9ff86836b969ff2bc537f8
JeffreyEarly <jeffrey@earlyinnovations.com>
2015-12-09 20:51:08 -0800

    Forgot to add autocorrelation function

---
+++ Autocorrelation.m
@@ -0,0 +1,12 @@
+function AC = Autocorrelation(v,maxlag)
+
+% v = v-mean(v);
+% sigma2 = std(v).^2;
+sigma2 = mean(v.*v);
+AC = zeros(size(v));
+for lag=0:maxlag;
+    v_shift = circshift(v,-lag,1);
+    v2 = v.*v_shift;
+    AC(lag+1) = mean(v2(1:(length(v)-lag)));
+end
+AC = AC(1:(maxlag+1))/sigma2;
