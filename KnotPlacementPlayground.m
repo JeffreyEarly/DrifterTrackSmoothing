649717903ac5aa853db799f022ef524448f3ff43
JeffreyEarly <jeffrey@earlyinnovations.com>
2015-12-18 15:22:28 -0800

    Improving knot placement to fractional indices

--- KnotPlacementPlayground.m
+++ KnotPlacementPlayground.m
@@ -1,8 +1,41 @@
-K = 4;
+K = 6;
 S = K-1;
-t_knot = linspace(0,10,11)';
+t_data = linspace(0,10,11)';
 
-t_data = t_knot;
+N = length(t_data);
+M = 11;
+
+t_knot = zeros(M,1);
+for i=1:M
+    tau_center = (N/M)*i;
+    half_width = (N/M)*(K-2)/2;
+    if (tau_center-half_width < 1)
+        half_width = tau_center-1;
+    elseif (tau_center+half_width>N)
+        half_width = N-tau_center;
+    end
+    
+    index_start = tau_center-half_width;
+    if (mod(index_start) ~= 0 )
+       frac_start =  1-mod(index_end);
+       index_start = floor(index_end);
+    end
+    
+    index_end = tau_center+half_width;
+    if (mod(index_end) ~= 0 )
+       frac_end =  mod(index_end);
+       index_end = ceil(index_end);
+    end
+    
+    range = (tau_center-half_width):1:(tau_center+half_width);
+    
+    ceil(range(end))
+    
+    t_knot(i) = mean( t_data(range) );
+end
+
+
+return
 
 % rows are t_data, columns are number of splines
 B = bspline(t_data,t_knot,K);
