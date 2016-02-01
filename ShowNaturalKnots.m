d6c1406d12ab0223f6a3ce0e07b2ca42881ddb84
JeffreyEarly <jeffrey@jeffreyearly.com>
2016-02-01 14:48:59 -0800

    Draft of figure

--- ShowNaturalKnots.m
+++ ShowNaturalKnots.m
@@ -1,4 +1,4 @@
-t = (0:6)';
+t = (0:10)';
 x = sin(2*pi*t/4);
 sigma = 1;
 dx = sigma*ones(size(x));
@@ -22,21 +22,57 @@ figure
 tq = linspace(t(1),t(end),10*N)';
 Bq = bspline(tq,t_knot,S+1);
 
-subplot(3,1,1)
+totalPlots = 5;
+subplot(totalPlots,1,1)
 plot(tq,squeeze(Bq(:,:,1))*m_x,'b','LineWidth',1.5), hold on
 scatter(t,squeeze(B(:,:,1))*m_x,6^2,'filled')
-vlines(t_knot0, 'g--')
+vlines(t_knot, 'g--')
 
-return;
 
-subplot(3,1,2)
-plot(t,x_true,'k','LineWidth',1), hold on
-plot(tq1,x_fit1,'b','LineWidth',1.5)
-scatter(t,x,6^2,'filled')
-vlines(t_knot1, 'g--')
+K = 2;
+S = K-1;
+[t_knot] = NaturalKnotsForSpline( t, K );
+[m_x,Cm_x,B] = bspline_fit_no_tension(t,x,dx,S,t_knot,w);
+Bq = bspline(tq,t_knot,S+1);
 
-subplot(3,1,3)
-plot(t,x_true,'k','LineWidth',1), hold on
-plot(tq2,x_fit2,'b','LineWidth',1.5)
-scatter(t,x,6^2,'filled')
-vlines(t_knot2, 'g--')
+
+subplot(totalPlots,1,2)
+plot(tq,squeeze(Bq(:,:,1))*m_x,'b','LineWidth',1.5), hold on
+scatter(t,squeeze(B(:,:,1))*m_x,6^2,'filled')
+vlines(t_knot, 'g--')
+
+
+
+
+K = 3;
+S = K-1;
+[t_knot] = NaturalKnotsForSpline( t, K );
+[m_x,Cm_x,B] = bspline_fit_no_tension(t,x,dx,S,t_knot,w);
+Bq = bspline(tq,t_knot,S+1);
+
+subplot(totalPlots,1,3)
+plot(tq,squeeze(Bq(:,:,1))*m_x,'b','LineWidth',1.5), hold on
+scatter(t,squeeze(B(:,:,1))*m_x,6^2,'filled')
+vlines(t_knot, 'g--')
+
+K = 4;
+S = K-1;
+[t_knot] = NaturalKnotsForSpline( t, K );
+[m_x,Cm_x,B] = bspline_fit_no_tension(t,x,dx,S,t_knot,w);
+Bq = bspline(tq,t_knot,S+1);
+
+subplot(totalPlots,1,4)
+plot(tq,squeeze(Bq(:,:,1))*m_x,'b','LineWidth',1.5), hold on
+scatter(t,squeeze(B(:,:,1))*m_x,6^2,'filled')
+vlines(t_knot, 'g--')
+
+K = 5;
+S = K-1;
+[t_knot] = NaturalKnotsForSpline( t, K );
+[m_x,Cm_x,B] = bspline_fit_no_tension(t,x,dx,S,t_knot,w);
+Bq = bspline(tq,t_knot,S+1);
+
+subplot(totalPlots,1,5)
+plot(tq,squeeze(Bq(:,:,1))*m_x,'b','LineWidth',1.5), hold on
+scatter(t,squeeze(B(:,:,1))*m_x,6^2,'filled')
+vlines(t_knot, 'g--')
